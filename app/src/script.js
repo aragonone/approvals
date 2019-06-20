c2f63982e04549b15f2dea87e866d9dc87811dc6
Gorka <gorka@aquigorka.com>
2019-06-20 16:23:16 +0200

    Approvals: add boilerplate app

---
+++ app/src/script.js
@@ -0,0 +1,37 @@
+import Aragon, { events } from '@aragon/api'
+import { addressesEqual } from './web3-utils'
+import voteSettings from './vote-settings'
+import { voteTypeFromContractEnum } from './vote-utils'
+import { EMPTY_CALLSCRIPT } from './evmscript-utils'
+import tokenDecimalsAbi from './abi/token-decimals.json'
+import tokenSymbolAbi from './abi/token-symbol.json'
+
+const tokenAbi = [].concat(tokenDecimalsAbi, tokenSymbolAbi)
+
+const app = new Aragon()
+
+
+async function initialize(tokenAddr) {
+  return app.store(
+    (state, { event, returnValues }) => {
+      const nextState = {
+        ...state,
+      }
+
+      switch (event) {
+        case '':
+          return nextState
+        default:
+          return nextState
+      }
+    },
+    { init: initState(tokenAddr) }
+  )
+}
+
+const initState = tokenAddr => async cachedState => {
+  return {
+    ...cachedState,
+    isSyncing: true,
+  }
+}
